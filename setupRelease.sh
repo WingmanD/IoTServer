eccb89ed9948e4a4d1bdfd6bab749a0676e1eaf1
WingmanD <cemeljicdavid@gmail.com>
2023-06-04 11:33:03 +0200

    Updated setupRelease.sh script

--- setupRelease.sh
+++ setupRelease.sh
@@ -1,10 +1,11 @@
 ﻿#!/bin/bash
+
 BUILD_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$BUILD_PATH/build"
 
 conan install . --build=missing -s cppstd=20 -s build_type=Release
 
-cmake . -G Ninja
+cmake -G "Unix Makefiles" .
 
 cmake --build . --config Release
